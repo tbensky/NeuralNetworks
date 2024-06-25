fix normalization

ANNLoops/ann_proj.py
@@ -11,7 +11,7 @@ nn = neural_net.neural_net(
             input_neuron_count=1,
             output_neuron_count=4,
             hidden_neuron_count=[10,5],
-            learning_rate=0.5,
+            learning_rate=0.1,
             init_neuron_bias=0.01
             )
 
@@ -48,10 +48,12 @@ while True:
         out = nn.forward(input)
         #format of out: out[0]=x, out[1]=y, out[2]=vx, out[3]=vy
         L = L + 0.5 * sum([(out[i] - output[i])**2 for i in range(len(output))])
-        v = math.sqrt(out[2]**2+out[3]**2)
+        vx = out[2] * v0
+        vy = out[2] * v0
+        v = math.sqrt(vx**2+vy**2)
         deriv = nn.get_deriv()
-        xpp = deriv['app'][0]
-        ypp = deriv['app'][1]
+        xpp = deriv['app'][0] * dmax
+        ypp = deriv['app'][1] * dmax
         C = nn.W(0,0,0)
         g = 9.8
         L = L + (xpp - C*v*out[2])**2